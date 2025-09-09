4602adc7e72f6a8c8f1de1732c895ec91f469e03
FoxxMGR <magomacievgadzimurad@gmail.com>
2025-09-09 16:36:21 +0300

    третий шаг

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const  welcomeUser = () => {
+    const name = readlineSync.question('Ваше имя: ');
+    console.log('Hello, ' + name + '!')
+}
+export default welcomeUser;
