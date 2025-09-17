897b4312742c435511d27d3824aecd4e1f1e3240
FoxxMGR <magomacievgadzimurad@gmail.com>
2025-09-17 15:44:57 +0300

    исправил "Ваше имя:" на "May I have your name?"

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync'
 const welcomeUser = () => {
-  const name = readlineSync.question('Ваше имя: ')
+  const name = readlineSync.question('May I have your name? ')
   console.log('Hello, ' + name + '!')
   return name
 }
